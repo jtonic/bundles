bb0bfa0dd49059e1727693489f145a6a9d879e9a
jtonic <antonel.pazargic@gmail.com>
2017-10-15 19:10:11 +0300

    Simple property delegation. Simple registration of a value in task extra

--- tutorials/kt/oop/build.gradle.kts
+++ tutorials/kt/oop/build.gradle.kts
@@ -7,7 +7,6 @@ apply {
     plugin("application")
 }
 
-
 application {
     mainClassName = "ro.jtonic.tutorials.kt.oop.SimpleTest"
 }
@@ -15,3 +14,19 @@ application {
 dependencies {
     testCompile(project(":tutorials:kt:model"))
 }
+
+val myTask = task("myTask") {
+
+    val foo by extra { 42 }
+
+    doLast {
+        println("Extra property value: $foo")
+    }
+}
+
+val foo: Int by myTask.extra
+afterEvaluate {
+    println("myTask.foo = $foo")
+}
+
+defaultTasks(myTask.name)
