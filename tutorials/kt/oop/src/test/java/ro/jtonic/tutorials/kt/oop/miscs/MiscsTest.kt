1d06cb05ee8bdeae75db7aeb797419e4e21d037d
jtonic <antonel.pazargic@gmail.com>
2017-11-27 08:26:35 +0200

    kotlin v. 1.1.61. Nullable types handling

--- tutorials/kt/oop/src/test/java/ro/jtonic/tutorials/kt/oop/miscs/MiscsTest.kt
+++ tutorials/kt/oop/src/test/java/ro/jtonic/tutorials/kt/oop/miscs/MiscsTest.kt
@@ -1,5 +1,6 @@
 package ro.jtonic.tutorials.kt.oop.miscs
 
+import io.kotlintest.matchers.shouldBe
 import org.junit.Test
 
 class MiscsTest {
@@ -13,4 +14,25 @@ class MiscsTest {
     a = "Antonel"
     a?.let {println("This is not null. $it")}
   }
+
+  @Test
+  fun `nullable tests`() {
+    val a1: String? = null
+    val a2 = a1 ?: ""
+
+    a2 shouldBe  ""
+
+    val a3 = a1?.let { println("a1 is not null"); a1.reversed() }
+    a3 shouldBe null
+
+    val a4 = "jtonic"
+
+    val a5 = a4?.let {
+      println("a4 is not null")
+      a4.reversed()
+    }
+
+    a5 shouldBe a4.reversed()
+  }
+
 }
