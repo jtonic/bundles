f851b4186bc938efd3158c7afd1c23d07a0207a7
jtonic <antonel.pazargic@gmail.com>
2017-12-17 11:15:02 +0200

    reproduce the issue KT-21862

---
+++ tutorials/kt/src/test/java/ro/jtonic/tutorials/kt/issues/Session.kt
@@ -0,0 +1,32 @@
+package ro.jtonic.tutorials.kt.issues
+
+import io.kotlintest.matchers.shouldThrow
+import org.junit.Test
+
+class Session {
+
+    companion object INSTANCE {
+
+        private lateinit var session: Session
+
+        fun createSession() = Session().apply {
+            session = this
+        }
+
+        fun weird() {
+            INSTANCE::session.isAbstract // this works
+            INSTANCE::session.isInitialized // java.lang.NoSuchFieldError: session ???!!!
+        }
+    }
+}
+
+class SessionTest {
+
+    @Test
+    fun `reproduce the issue KT-21862`() {
+        Session.createSession()
+        shouldThrow<NoSuchFieldError> {
+            Session.weird()
+        }
+    }
+}
