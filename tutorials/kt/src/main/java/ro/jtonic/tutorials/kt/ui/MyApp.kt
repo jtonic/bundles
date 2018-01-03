c4f0007a87dad6db7f8c5478a8efdd007cdcb53f
jtonic <antonel.pazargic@gmail.com>
2018-01-03 07:38:58 +0200

    Simple form layout example.

--- tutorials/kt/src/main/java/ro/jtonic/tutorials/kt/ui/MyApp.kt
+++ tutorials/kt/src/main/java/ro/jtonic/tutorials/kt/ui/MyApp.kt
@@ -3,6 +3,8 @@ package ro.jtonic.tutorials.kt.ui
 import javafx.application.Application
 import javafx.collections.FXCollections
 import javafx.geometry.Pos
+import javafx.scene.control.DatePicker
+import javafx.scene.control.TextField
 import javafx.scene.layout.VBox
 import javafx.scene.paint.Color
 import tornadofx.*
@@ -11,9 +13,9 @@ import tornadofx.*
  * Created by Antonel Ernest Pazargic on 24/12/2017.
  * @author Antonel Ernest Pazargic
  */
-class MyApp : App(MyView::class)
+class MyApp : App(MyView2::class)
 
-class MyView : View() {
+class MyView1 : View() {
 
     override val root: VBox = vbox {
         hbox {
@@ -38,6 +40,32 @@ class MyView : View() {
             alignment = Pos.CENTER
             action {
                 println("Logged in!!!")
+            }
+        }
+    }
+}
+
+class MyView2 : View() {
+
+    override val root = form {
+        lateinit var firstNameTxt: TextField
+        lateinit var lastNameTxt: TextField
+        lateinit var birthDateDatePick: DatePicker
+
+        fieldset("Personal info") {
+            field("First name:") {
+                firstNameTxt = textfield { id = "firstNameTxt" }
+            }
+            field("Last name:") {
+                lastNameTxt = textfield { id = "lastNameTxt" }
+            }
+            field("Birthday:") {
+                birthDateDatePick = datepicker()
+            }
+        }
+        button("Save") {
+            action {
+                println("${firstNameTxt.text} ${lastNameTxt.text}, born at ${birthDateDatePick.value} was saved!!!!")
             }
         }
     }
