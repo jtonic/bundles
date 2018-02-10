65fb9ccc2bb153e2d82273862954cc014fe8cbbb
jtonic <antonel.pazargic@gmail.com>
2018-02-10 14:43:27 +0200

    vetoable and observable property delegates by example

--- tutorials/kt/src/test/java/ro/jtonic/tutorials/kt/tutorials/DelegatesTest.kt
+++ tutorials/kt/src/test/java/ro/jtonic/tutorials/kt/tutorials/DelegatesTest.kt
@@ -1,5 +1,7 @@
 package ro.jtonic.tutorials.kt.tutorials
 
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.matchers.shouldNotBe
 import org.junit.Test
 import kotlin.properties.Delegates
 
@@ -10,9 +12,41 @@ import kotlin.properties.Delegates
 class DelegatesTest {
 
     @Test
-    fun `simple delegate`() {
-        var a: String by Delegates.observable("", onChange = {_, oldValue, newValue-> println("$oldValue -> $newValue")})
+    fun `some simple delegates - vetoable and observable`() {
+        var a: String by Delegates.observable("", onChange = { _, oldValue, newValue -> println("$oldValue -> $newValue") })
         a = "a"
         a = "b"
+
+
+        var jtonic: Student by Delegates.vetoable(Student("Antonel"))
+        { _, _, newValue ->
+            if (newValue.age < 18) {
+                println("The student should be at least 18 years old.")
+                return@vetoable false
+            }
+            true
+        }
+
+        var ligia: Student by Delegates.vetoable(Student("Ligia", age = 4))
+        { _, _, newValue ->
+            if (newValue.age < 18) {
+                println("The student should be at least 18 years old.")
+                return@vetoable false
+            }
+            return@vetoable true
+        }
+
+        jtonic = Student("Antonel-Ernest", 27)
+        jtonic.age shouldBe 27
+
+        ligia = Student("Ligia Pazargic", 5)
+
+        println("ligia = ${ligia.name} ${ligia.age}")
+
+        ligia.age shouldNotBe 5
+        ligia.name shouldNotBe "Ligia Pazargic"
     }
+
+    class Student(val name: String, val age: Int = 5)
 }
+
