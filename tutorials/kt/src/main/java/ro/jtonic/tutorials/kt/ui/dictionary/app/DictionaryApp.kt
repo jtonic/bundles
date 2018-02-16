a3fa62dfa06b99e4dba4930d9890e1729f55cc54
jtonic <antonel.pazargic@gmail.com>
2018-02-16 23:39:23 +0200

    fxml based layout in TornadoFX

--- tutorials/kt/src/main/java/ro/jtonic/tutorials/kt/ui/dictionary/app/DictionaryApp.kt
+++ tutorials/kt/src/main/java/ro/jtonic/tutorials/kt/ui/dictionary/app/DictionaryApp.kt
@@ -2,14 +2,14 @@ package ro.jtonic.tutorials.kt.ui.dictionary.app
 
 import javafx.application.Application
 import ro.jtonic.tutorials.kt.ui.dictionary.css.Style
-import ro.jtonic.tutorials.kt.ui.dictionary.view.WordsView
+import ro.jtonic.tutorials.kt.ui.dictionary.view.MainView
 import tornadofx.*
 
 /**
  * Created by Antonel Ernest Pazargic on 24/12/2017.
  * @author Antonel Ernest Pazargic
  */
-class DictionaryApp : App(WordsView::class, Style::class) {
+class DictionaryApp : App(MainView::class, Style::class) {
     init {
         Rest.useApacheHttpClient()
     }
---
+++ tutorials/kt/src/main/java/ro/jtonic/tutorials/kt/ui/dictionary/view/MainView.kt
@@ -0,0 +1,25 @@
+package ro.jtonic.tutorials.kt.ui.dictionary.view
+
+import javafx.beans.property.SimpleIntegerProperty
+import javafx.scene.control.Label
+import javafx.scene.layout.BorderPane
+import tornadofx.*
+
+/**
+ * Created by Antonel Ernest Pazargic on 16/02/2018.
+ * @author Antonel Ernest Pazargic
+ */
+class MainView : View("My View") {
+
+    override val root: BorderPane by fxml("/views/MainView.fxml")
+    val counter = SimpleIntegerProperty()
+    val counterLabel: Label by fxid()
+
+    init {
+        counterLabel.bind(counter)
+    }
+
+    fun increment() {
+        counter.value += 1
+    }
+}
