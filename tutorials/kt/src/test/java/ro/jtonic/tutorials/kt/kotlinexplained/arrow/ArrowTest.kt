d3286b48ee926509bc4576e05befb2fd79b0251f
jtonic <antonel.pazargic@gmail.com>
2018-04-08 10:29:21 +0300

    Complement

--- tutorials/kt/src/test/java/ro/jtonic/tutorials/kt/kotlinexplained/arrow/ArrowTest.kt
+++ tutorials/kt/src/test/java/ro/jtonic/tutorials/kt/kotlinexplained/arrow/ArrowTest.kt
@@ -1,6 +1,6 @@
 package ro.jtonic.tutorials.kt.kotlinexplained.arrow
 
-import arrow.syntax.function.curried
+import arrow.syntax.function.complement
 import arrow.syntax.function.pipe
 import io.kotlintest.matchers.shouldBe
 import org.junit.Test
@@ -12,13 +12,12 @@ import org.junit.Test
 
 class ArrowTest {
 
-    fun sum(a: Int, b: Int = 1) = a + b
-    fun format(a: Int) = "$a"
-
     @Test
-    fun `test andThen`() {
-        val inc = ::sum.curried()(1)
-        2 pipe inc pipe ::format shouldBe "3"
-    }
+    fun `test complement`() {
 
+        val even: (Int) -> Boolean = { it % 2 == 0}
+        10 pipe even shouldBe true
+
+        11 pipe even.complement() shouldBe true
+    }
 }
