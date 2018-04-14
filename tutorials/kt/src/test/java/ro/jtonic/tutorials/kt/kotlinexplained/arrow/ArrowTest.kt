78f53ea95821c837c6e2ffd3ed6f86d17eacc9c9
jtonic <antonel.pazargic@gmail.com>
2018-04-14 18:05:28 +0300

    [p2] tupleX in arrow

--- tutorials/kt/src/test/java/ro/jtonic/tutorials/kt/kotlinexplained/arrow/ArrowTest.kt
+++ tutorials/kt/src/test/java/ro/jtonic/tutorials/kt/kotlinexplained/arrow/ArrowTest.kt
@@ -1,5 +1,8 @@
 package ro.jtonic.tutorials.kt.kotlinexplained.arrow
 
+import arrow.core.toT
+import arrow.syntax.tuples.plus
+import io.kotlintest.matchers.shouldBe
 import org.junit.Test
 
 /**
@@ -7,10 +10,27 @@ import org.junit.Test
  * @author Antonel Ernest Pazargic
  */
 
+typealias Age = Int
+typealias SiblingsNo = Int
+typealias Salary = Int
+
 class ArrowTest {
 
     @Test
-    fun `reset`() {
+    fun `the beauty of working with arrow TupleX`() {
+
+        val salary: Salary = 100_000
+        val age: Age = 48
+        val siblingsNo: SiblingsNo = 3
+
+        val t2 = "Antonel" toT salary
+
+        val t3 = t2 + age
+        val t4 = t3 + siblingsNo
+
+        t2.run { a shouldBe "Antonel"; b shouldBe salary }
+        t3.c shouldBe age
+        t4.d shouldBe siblingsNo
 
     }
 }
