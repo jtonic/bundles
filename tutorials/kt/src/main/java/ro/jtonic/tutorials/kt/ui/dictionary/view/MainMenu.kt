de14c0e3a02b97034feac337eded91fb888afe45
jtonic <antonel.pazargic@gmail.com>
2018-04-15 12:35:22 +0300

    [tornadofx] controlsfx notification

--- tutorials/kt/src/main/java/ro/jtonic/tutorials/kt/ui/dictionary/view/MainMenu.kt
+++ tutorials/kt/src/main/java/ro/jtonic/tutorials/kt/ui/dictionary/view/MainMenu.kt
@@ -1,6 +1,7 @@
 package ro.jtonic.tutorials.kt.ui.dictionary.view
 
 import javafx.application.Platform
+import org.controlsfx.control.Notifications
 import tornadofx.*
 
 /**
@@ -13,6 +14,19 @@ class MainMenu : UIComponent() {
         menu("File") {
             item("Exit", "Shortcut+X").action {
                 Platform.exit()
+            }
+        }
+        menu("Examples") {
+            item("Notification").action {
+                Notifications.create().apply {
+                    title("Information")
+                    text("The AT has successfully generated.\nClick to get to the result.")
+                    onAction {
+                        println("This is the result...")
+                    }
+                }.also {
+                    it.showInformation()
+                }
             }
         }
         menu("Help") {
