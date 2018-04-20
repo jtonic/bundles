ff781a00095f018f77f13ad3d9f599b2b45f72f0
jtonic <antonel.pazargic@gmail.com>
2018-04-20 17:38:16 +0300

    [p2] arrow lift for composing pure with inpure functions with pipe

--- tutorials/kt/src/test/java/ro/jtonic/tutorials/kt/kotlinexplained/arrow/ArrowTest.kt
+++ tutorials/kt/src/test/java/ro/jtonic/tutorials/kt/kotlinexplained/arrow/ArrowTest.kt
@@ -1,6 +1,11 @@
 package ro.jtonic.tutorials.kt.kotlinexplained.arrow
 
 import arrow.core.toT
+import arrow.data.Success
+import arrow.data.Try
+import arrow.data.ev
+import arrow.data.functor
+import arrow.syntax.function.pipe
 import arrow.syntax.tuples.plus
 import io.kotlintest.matchers.shouldBe
 import org.junit.Test
@@ -32,5 +37,23 @@ class ArrowTest {
         t3.c shouldBe age
         t4.d shouldBe siblingsNo
 
+    }
+
+    @Test
+    fun `composition of pure and inpure functions`() {
+
+        fun pure1(i: Int) = i + 1
+
+        fun inpure2(i: Int) : Try<Int> {
+            return Success(i + 1)
+        }
+
+        val pure3 = ::pure1
+
+        fun pure4(i: Int) = i + 1
+
+        with(Try.functor()) {
+            1 pipe ::pure1 pipe ::inpure2 pipe lift(pure3) pipe lift(::pure4)
+        }.ev().fold({ "not found" }) { it } shouldBe 5
     }
 }
