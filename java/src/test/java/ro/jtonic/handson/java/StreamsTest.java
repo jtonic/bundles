00aad07e9b55314ab0b6574ee6601f9721cb81b4
jtonic <antonel.pazargic@gmail.com>
2018-06-07 06:46:52 +0300

    [java] [streams] max final stream method with comparing static method.

--- java/src/test/java/ro/jtonic/handson/java/StreamsTest.java
+++ java/src/test/java/ro/jtonic/handson/java/StreamsTest.java
@@ -1,10 +1,14 @@
 package ro.jtonic.handson.java;
 
 import static java.util.stream.Collectors.toList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 import org.junit.Assert;
 import org.junit.Test;
@@ -22,5 +26,12 @@ public class StreamsTest {
                                           .flatMap(Collection::stream)
                                           .collect(toList());
     Assert.assertEquals(Arrays.asList(1, 2, 3, 4), collected);
+  }
+
+  @Test
+  public void testSimpleMaxFinalOperator() {
+    final Optional<String> max = Stream.of("Antonel", "Irina", "Oana", "Ion", "Liviu").max(Comparator.comparing(name -> name.length()));
+    assertTrue(max.isPresent());
+    assertEquals("Antonel", max.get());
   }
 }
